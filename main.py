726a093fdd9e406522a72720c0e3217ebae81f75
StefanoZanoni <stefano.zanoni00@gmail.com>
2024-06-20 12:25:43 +0200

    Split tests from the algorithm

--- main.py
+++ main.py
@@ -13,31 +13,6 @@ from src.cqp import BCQP
 from src.frank_wolfe import frank_wolfe
 from src.qp import QP
 
-if not os.path.exists(f'tests'):
-    os.makedirs(f'tests')
-
-
-def store_results(results: dict, dirname: str):
-    """
-    Store the results in a given directory.
-
-    :param results: The results to store.
-    :param dirname: The name of the directory in which the files need to be stored.
-    """
-    with open(f'{dirname}/results.json', 'w') as f:
-        json.dump({'execution_time': results['execution_time'], 'iterations': results['iterations']}, f)
-    with open(f'{dirname}/configuration.json', 'w') as f:
-        json.dump({'dimensions': int(results['dimensions']), 'rank': int(results['rank']),
-                   'eccentricity': float(results['eccentricity']),
-                   'active_constraints_percentage': float(results['active'])}, f)
-
-    for i, gaps in enumerate(results['all_gaps']):
-        plt.plot(gaps)
-        plt.xlabel('Iteration')
-        plt.ylabel('Gap')
-        plt.savefig(f'{dirname}/subproblem_{i}_gap.png')
-        plt.close()
-
 
 def solve(problem: QP, constraints: list[BoxConstraints], As: list[np.ndarray], n: int, verbose: int = 1) -> \
         tuple[np.ndarray, float, list, list[list]]:
@@ -82,27 +57,49 @@ def solve(problem: QP, constraints: list[BoxConstraints], As: list[np.ndarray], 
 
 
 def main():
-    test_dimensions = np.arange(10, 1010, 10)
-    for n in test_dimensions:
-        if not os.path.exists(f'tests/dimension_{n}'):
-            os.makedirs(f'tests/dimension_{n}')
+    parser = argparse.ArgumentParser(description='This is a program to solve optimization problems using the '
+                                                 'Frank-Wolfe algorithm.')
+    parser.add_argument('-n', '--dimensions',
+                        type=int,
+                        default=10,
+                        help='The dimension of the problem. Default is 10.')
+    parser.add_argument('-r', '--rank',
+                        type=int,
+                        default=10,
+                        help='The rank of the matrix Q. Default is 10.')
+    parser.add_argument('-e', '--eccentricity',
+                        type=float,
+                        default=0.9,
+                        help='The eccentricity of the matrix Q. Default is 0.9.')
+    parser.add_argument('-a', '--active',
+                        type=float,
+                        default=1.0,
+                        help='The active constraints percentage of the problem. Default is 1.0.')
+    parser.add_argument('-v', '--verbose',
+                        type=int,
+                        default=1,
+                        help='The verbosity level. Default is 1.')
+    args = parser.parse_args()
 
-    for n in tqdm(test_dimensions):
-        Is = create_index_sets(n, uniform=False)
-        As = [create_A(n, I) for I in Is]
-        b = create_b()
+    n = args.n
+    rank = args.rank
+    eccentricity = args.eccentricity
+    active = args.active
+    verbose = args.verbose
+    if verbose != 0:
+        verbose = 1
 
-        constraints = [BoxConstraints(A, b, ineq=True) for A in As]
+    Is = create_index_sets(n, uniform=False)
+    As = [create_A(n, I) for I in Is]
+    b = create_b()
 
-        eccentricity = np.random.uniform(0.1, 1)
-        rank = np.random.randint(1, n + 1)
-        active = np.random.uniform(0.1, 1)
-        problem = QP(n, rank=rank, eccentricity=eccentricity, active=active, c=False)
+    constraints = [BoxConstraints(A, b, ineq=True) for A in As]
+    problem = QP(n, rank=rank, eccentricity=eccentricity, active=active, c=False)
 
-        _, execution_time, iterations, all_gaps = solve(problem, constraints, As, n, verbose=0)
-        store_results({'execution_time': execution_time, 'iterations': iterations, 'all_gaps': all_gaps,
-                       'dimensions': n, 'rank': rank, 'eccentricity': eccentricity, 'active': active},
-                      f'tests/dimension_{n}')
+    _, execution_time, iterations, _ = solve(problem, constraints, As, n, verbose=verbose)
+
+    print(f"Execution Time: {execution_time * 1000} ms")
+    print(f"Iterations for each sub-problem: {iterations}")
 
 
 if __name__ == '__main__':
